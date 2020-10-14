517ea772b66c17e2ee38d20b4c3deebc5268edc7
farafonoff <farafonoff@gmail.com>
2020-10-14 21:39:30 +0400

    updated port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const fetch = require('node-fetch')
 const app = express()
-const port = 3000
+const port = 3111
 const PrometheusFormatter = require('./prometheusFormatter');
 const NetisParser = require('./netisParser');
 
